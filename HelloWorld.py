d4c90864f8f3799b19130c19ac30ecfdc76275eb
RickC <pygator@linux.com>
2016-10-30 08:15:49 -0400

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+__author__ = 'dev'
+print('Hello, World!')
+print(1+2)
+print("The end")
+greeting = "Hello "
+name = "Rick"
+print(greeting + name)
+# this is a comment
+name = input("Please enter your name:")
+print(greeting+" "+ name)
---
+++ tempchu.py
@@ -0,0 +1,6 @@
+good_num = [3,5,7,8,0]
+mynum=7
+
+for mynum in good_num:
+    if mynum in good_num:
+      print("You guessed a good number!")
